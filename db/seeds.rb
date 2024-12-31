52a47ff46d9ed86b6fe069b730d6cbad1407c68c
MikeNix12 <mtnix12@tamu.edu>
2024-12-31 10:35:33 +0800

    first attempt

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,13 @@ more_movies = [
   { title: 'Nomadland', rating: 'R',
     release_date: '19-Feb-2021' },
   { title: 'CODA', rating: 'PG-13',
-    release_date: '13-Aug-2021' }
+    release_date: '13-Aug-2021' },
+  { title: 'Gravity', rating: 'PG-13',
+  release_date: '04-Oct-2013' },
+  { title: 'Interstellar', rating: 'PG-13',
+  release_date: '7-Nov-2014' },
+  { title: 'The Room', rating: 'M-18',
+  release_date: '27-Jun-2003' }
 ]
 
 more_movies.each do |movie|
